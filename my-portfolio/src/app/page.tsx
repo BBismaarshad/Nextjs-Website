17ea381ea0abc74a7fafe8064c0c29a2d75f2c77
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2024-11-10 08:54:01 +0500

    Add files via upload

---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from "next/image";
+import Hero from  "@/components/Hero"
+
+export default function Home() {
+  return (
+   
+        <div>
+         
+          <Hero />
+          
+        </div>
+  );
+}
+
