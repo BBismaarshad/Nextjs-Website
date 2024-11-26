d9cb462a16c5e02150cac3a879ea9608b35ae082
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2024-11-26 11:50:33 +0500

    Add files via upload

---
+++ book management system/app/page.tsx
@@ -0,0 +1,28 @@
+import BookCard from "@/components/book-card";
+import CreateBookForm from "@/components/book-form";
+
+import { Book } from "@/data";
+
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/books");
+  const data: Book[] = await res.json();
+  return (
+    <div className="bg-[#08313a]">
+      <h1 className="text-center pt-5 text-[#c1d9b7] font-extrabold text-xl">Books / Computer programming</h1>
+    <div className="container mx-auto px-4 gap-4 py-8">
+      <CreateBookForm  />
+      <div className=" grid grid-cols-1 gap-6 mt-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {data.map((book) => (
+          <BookCard
+            key={book.id}
+            author={book.author}
+            imgUrl={book.imgUrl}
+            title={book.title}
+            id={book.id}
+          />
+        ))}
+      </div>
+      </div>
+    </div>
+  );
+}
